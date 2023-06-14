a157d90d1c6e575eba782f2806368ea9648a77f0
Tarun Mittal <tm6622@g.rit.edu>
2023-06-14 16:09:07 -0400

    added more styled components for AboutSection

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -36,12 +36,27 @@ h2 {
 h3 {
     color: white;
 }
+
+h4 {
+    font-weight: bold;
+}
+
+a {
+    font-weight: 1.1rem;
+}
+
+span {
+    font-weight: bold;
+    color: #23d997;
+}
     
 p {
     padding: 3rem 0rem;
     color: #ccc;
     font-size: 1.4rem;
+    line-height: 150%;
 }
+
 
 `;
 
