6a44cdd2a77c21717dcb6238e173b40dd0791ccb
Tarun Mittal <tm6622@g.rit.edu>
2023-06-15 16:47:01 -0400

    added styles to moviedetail

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -30,10 +30,24 @@ const MovieDetail = () => {
 };
 
 const Details = styled.div`
-
+    color: white;
 `;
 
 const Headline = styled.div`
+    min-height: 90vh;
+    padding-top: 20vh;
+    position: relative;
+    h2 {
+        position: absolute;
+        top: 10%;
+        left: 50%;
+        transform: translate(-50%, -10%);
+    }
+    img {
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
 `;
 
 export default MovieDetail;
