bd011a3a1bea0711c7e67272bb11a849d83639ea
Tarun Mittal <tm6622@g.rit.edu>
2023-06-15 17:31:40 -0400

    added frame motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -8,30 +8,25 @@ import { motion } from "framer-motion";
 
 const AboutSection = () => {
     
-    const titleAnim = {
-        hidden: { opacity: 0 },
-        show: { opacity:1, transition: { duration: 2 } },
-    };
-
 
     return (
         <About>
             <Description>
-                <div className="title">
+                <motion.div>
                     <Hide>
-                        <motion.h2 variants={titleAnim} initial="hidden" animate="show">
+                        <motion.h2>
                             We work to make
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>
+                        <motion.h2>
                             your <span>dreams</span> come
-                        </h2>
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2>true.</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>Contact us for any photography or videography ideas that you have. 
                 We have professionals with amazing skills.</p>
                 <button>Contact Us</button>
