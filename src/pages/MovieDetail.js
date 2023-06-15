04dac8a230a954cb911a0811168fc83b835b35bf
Tarun Mittal <tm6622@g.rit.edu>
2023-06-15 16:43:45 -0400

    added movie details pages

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { MovieState } from '../movieState';
 import styled from "styled-components";
-import { useHistory } from "react-router-dom";
 
 const MovieDetail = () => {
+    const location = useLocation();
+    const url = location.pathname;
+    const [movies, setMovies] = useState(MovieState);
+    const [movie, setMovie] = useState(null);
+
+    //Use Effect
+    useEffect(() => {
+        const currentMovie = movies.filter((stateMovie) => stateMovie.url === url)
+        setMovie(currentMovie[0]);
+    }, [movies, url]);
+    
     return (
-        <div>
-            <h1>Movie Detail</h1>
-        </div>
+     <>
+      {movie && (
+        <Details>
+            <Headline>
+                <h2>{movie.title}</h2>
+                <img src={movie.mainImg} alt="movie" />
+            </Headline>
+        </Details>
+        )}
+     </>
     );
 };
+
+const Details = styled.div`
+
+`;
+
+const Headline = styled.div`
+`;
 
 export default MovieDetail;
