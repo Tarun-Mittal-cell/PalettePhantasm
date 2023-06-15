99c55dcecd069008de44fa9df757026d06b98fb5
Tarun Mittal <tm6622@g.rit.edu>
2023-06-15 15:53:28 -0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 // Import pages
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path='/' element={<AboutUs />} />
+        <Route path='/work' element={<OurWork />} />
+        <Route path ='/contact' element={<ContactUs />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
